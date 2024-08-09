fd37caa9d7e21866f5ac77009ab740913f437523
Pako Mercado <sissyyayle0517@hotmail.com>
2024-08-08 19:48:33 -0600

    change color on navbar and some buttons and grid position on logo

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,23 @@
 import Image from "next/image";
-import logo from "@/public/contaTools-logo.png";
+import logo from "@/public/logo_transparent.svg";
 
 export default function Home() {
   return (
-    <section>
-      <article className="max-w-7xl animate-fade-right">
-        <h1 className="mt-5 text-start text-6xl leading-snug lg:text-8xl lg:leading-normal">
+    <section className="grid grid-cols-1 gap-5 lg:grid-cols-4">
+      <article className="col-span-2 max-w-7xl animate-fade-right">
+        <h1 className="mt-5 px-3 text-start text-6xl leading-snug lg:text-8xl lg:leading-normal">
           Herramientas que ayudan a solucionar el día a día en la
           contabilidad...
         </h1>
-        <h3 className="mt-8 text-xl lg:text-3xl">
+        <h3 className="mt-2 px-3 text-xl lg:text-3xl">
           Navega por las diferentes herramientas.
         </h3>
       </article>
-      <article className="flex animate-fade-left justify-end">
+      <article className="col-span-2 justify-self-center lg:col-span-2 lg:place-self-start lg:justify-self-end">
         <Image
           src={logo}
-          width={500}
-          height={500}
+          width={896}
+          height={896}
           alt="logo image from company"
           priority={true}
         />
--- components/NavbarApp.jsx
+++ components/NavbarApp.jsx
@@ -3,11 +3,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Navbar } from "flowbite-react";
-import logo from "@/public/contaTools-logo.png";
+import logo from "@/public/logo_white.svg";
 
 export default function NavbarApp() {
   return (
-    <Navbar className="dark:text-white" rounded fluid>
+    <Navbar className="bg-cyan-700 text-white dark:bg-cyan-700" rounded fluid>
       <Navbar.Brand as={Link} href="/">
         <Image
           src={logo}
@@ -22,19 +22,25 @@ export default function NavbarApp() {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link className="dark:text-white" href="/">
+        <Navbar.Link className="text-white dark:text-white" href="/">
           Inicio
         </Navbar.Link>
-        <Navbar.Link className="dark:text-white" href="/quotation">
+        <Navbar.Link className="text-white dark:text-white" href="/quotation">
           Gen. Cotizaciones
         </Navbar.Link>
-        <Navbar.Link className="dark:text-white" href="/paybackInformation">
+        <Navbar.Link
+          className="text-white dark:text-white"
+          href="/paybackInformation"
+        >
           Reg. Devolución
         </Navbar.Link>
-        <Navbar.Link className="dark:text-white" href="/getCosts">
+        <Navbar.Link className="text-white dark:text-white" href="/getCosts">
           Reg. Gastos
         </Navbar.Link>
-        <Navbar.Link className="dark:text-white" href="/counterMoney">
+        <Navbar.Link
+          className="text-white dark:text-white"
+          href="/counterMoney"
+        >
           Cont. Dinero
         </Navbar.Link>
       </Navbar.Collapse>

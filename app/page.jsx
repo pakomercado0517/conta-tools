09341edce3506f54f1d5818a7ab43ee90cdaba47
Pako Mercado <sissyyayle0517@hotmail.com>
2024-10-21 11:39:45 -0600

    change slogan from web page

--- app/page.jsx
+++ app/page.jsx
@@ -3,17 +3,17 @@ import logo from "@/public/logo_transparent.svg";
 
 export default function Home() {
   return (
-    <section className="grid grid-cols-1 gap-5 lg:grid-cols-4">
-      <article className="col-span-2 max-w-7xl animate-fade-right">
-        <h1 className="mt-5 px-3 text-start text-6xl leading-snug lg:text-8xl lg:leading-normal">
-          Herramientas que ayudan a solucionar el día a día en la
-          contabilidad...
+    <section className="flex min-h-screen flex-col items-center justify-center lg:flex-row lg:justify-evenly">
+      <article className="max-w-7xl animate-fade-right">
+        <h1 className="mt-5 px-3 text-start text-4xl leading-snug lg:mt-0 lg:max-w-4xl lg:text-7xl lg:leading-normal">
+          ContaTools: Desde cotizaciones hasta el control de efectivo, con
+          facilidad.
         </h1>
-        <h3 className="mt-2 px-3 text-xl lg:text-3xl">
+        <h3 className="mt-2 px-3 text-start text-xl font-semibold lg:mt-10 lg:text-3xl">
           Navega por las diferentes herramientas.
         </h3>
       </article>
-      <article className="col-span-2 justify-self-center lg:col-span-2 lg:place-self-start lg:justify-self-end">
+      <article className="justify-self-center">
         <Image
           src={logo}
           width={896}

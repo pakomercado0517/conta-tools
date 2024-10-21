35f42780478ac4b81d9b72b0d4ebe2d547350ae6
Pako Mercado <sissyyayle0517@hotmail.com>
2024-10-21 15:01:39 -0600

    change styles from index page

--- app/page.jsx
+++ app/page.jsx
@@ -3,17 +3,17 @@ import logo from "@/public/logo_transparent.svg";
 
 export default function Home() {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center lg:flex-row lg:justify-evenly">
-      <article className="max-w-7xl animate-fade-right">
-        <h1 className="mt-5 px-3 text-start text-4xl leading-snug lg:mt-0 lg:max-w-4xl lg:text-7xl lg:leading-normal">
+    <section className="min-h-xl flex max-h-full flex-col items-end justify-center lg:flex-row lg:justify-evenly">
+      <article className="max-w-7xl animate-fade-right lg:mt-24">
+        <h1 className="mt-5 text-pretty px-3 text-start text-4xl leading-snug md:ml-8 lg:mt-0 lg:max-w-4xl lg:text-7xl lg:leading-normal">
           ContaTools: Desde cotizaciones hasta el control de efectivo, con
           facilidad.
         </h1>
-        <h3 className="mt-2 px-3 text-start text-xl font-semibold lg:mt-10 lg:text-3xl">
+        <h3 className="mt-2 px-3 text-start text-xl font-semibold md:ml-9 lg:mt-10 lg:text-3xl">
           Navega por las diferentes herramientas.
         </h3>
       </article>
-      <article className="justify-self-center">
+      <article className="max-w-md lg:mt-8">
         <Image
           src={logo}
           width={896}

12bfeae71de94be9519443a30df9eb250d06792a
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-02-10 21:44:31 -0600

    agegate footer with profile link, set auto year and make styles for put footer on bottom page

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={oswald.className}>
-        <NavbarApp />
-        <div className="animate-fade">{children}</div>
-        <FooterComponent />
+        <div className="grid min-h-[100dvh] grid-rows-[auto,1fr,auto]">
+          <NavbarApp />
+          <div className="animate-fade">{children}</div>
+          <FooterComponent />
+        </div>
       </body>
     </html>
   );

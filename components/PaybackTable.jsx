cca432fa8f6c548c67e740ab41bb05cfb5fd5d9e
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-03-15 11:14:44 -0600

    change table to show

--- components/PaybackTable.jsx
+++ components/PaybackTable.jsx
@@ -40,10 +40,10 @@ export default function PaybackTable({
           <Table.HeadCell>Monto</Table.HeadCell>
           <Table.HeadCell>Comisión</Table.HeadCell>
           <Table.HeadCell>Total</Table.HeadCell>
+          {isChecked && <Table.HeadCell>Concepto Descuento</Table.HeadCell>}
           <Table.HeadCell>
             <span className="sr-only">Editar</span>
           </Table.HeadCell>
-          {isChecked && <Table.HeadCell>Concepto Descuento</Table.HeadCell>}
         </Table.Head>
         <Table.Body className="divide-y">
           {data.length > 0 &&
@@ -51,13 +51,10 @@ export default function PaybackTable({
               <Table.Row key={index} className="border-gray-600">
                 <Table.Cell>{el?.empresa}</Table.Cell>
                 <Table.Cell>{formatNumber.format(el?.monto)}</Table.Cell>
-                <Table.Cell>
-                  {" "}
-                  {`-${formatNumber.format(el?.comision)}`}{" "}
-                </Table.Cell>
+                <Table.Cell>{`-${formatNumber.format(el?.comision)}`}</Table.Cell>
                 <Table.Cell>{formatNumber.format(el?.total)}</Table.Cell>
-                <Table.Cell className="flex gap-5">
-                  {" "}
+                {/* El botón se mueve al final de la fila */}
+                <Table.Cell className="text-right">
                   <Button
                     size="sm"
                     color="failure"
@@ -68,6 +65,7 @@ export default function PaybackTable({
                 </Table.Cell>
               </Table.Row>
             ))}
+
           {isChecked && (
             <>
               {discount.map((d, index) => (
@@ -79,6 +77,7 @@ export default function PaybackTable({
                   <Table.Cell className="text-red-500">{d.concept}</Table.Cell>
                 </Table.Row>
               ))}
+              {/* La fila de "Suma Total" ahora es la penúltima */}
               <Table.Row className="border-gray-600">
                 <Table.Cell></Table.Cell>
                 <Table.Cell></Table.Cell>
@@ -86,15 +85,20 @@ export default function PaybackTable({
                   Suma Total
                 </Table.Cell>
                 <Table.Cell>{formatNumber.format(totalSum)}</Table.Cell>
+                <Table.Cell></Table.Cell>
               </Table.Row>
             </>
           )}
+
+          {/* Fila vacía movida a la última posición */}
           <Table.Row className="border-gray-600">
             <Table.Cell></Table.Cell>
             <Table.Cell></Table.Cell>
             <Table.Cell></Table.Cell>
             <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
           </Table.Row>
+
           {!isChecked && (
             <Table.Row className="border-gray-600">
               <Table.Cell></Table.Cell>
@@ -105,40 +109,6 @@ export default function PaybackTable({
               <Table.Cell>{formatNumber.format(totalSum)}</Table.Cell>
             </Table.Row>
           )}
-          {/* {isChecked && (
-            <>
-              <Table.Row>
-                <Table.Cell className="bg-gray-50 font-semibold dark:bg-gray-700"></Table.Cell>
-                <Table.Cell className="bg-gray-50 font-semibold dark:bg-gray-700"></Table.Cell>
-                <Table.Cell className="bg-gray-50 font-semibold dark:bg-gray-700">
-                  Fecha
-                </Table.Cell>
-                <Table.Cell className="bg-gray-50 font-semibold dark:bg-gray-700">
-                  Monto
-                </Table.Cell>
-                <Table.Cell className="bg-gray-50 font-semibold dark:bg-gray-700">
-                  <span className="sr-only">Concepto</span>
-                </Table.Cell>
-              </Table.Row>
-              {discount.map((d, index) => (
-                <Table.Row className="border-gray-600" key={index}>
-                  <Table.Cell></Table.Cell>
-                  <Table.Cell></Table.Cell>
-                  <Table.Cell className="text-red-500">{d.date}</Table.Cell>
-                  <Table.Cell className="text-red-500">{`-${formatNumber.format(d.total)}`}</Table.Cell>
-                  <Table.Cell className="text-red-500">{d.concept}</Table.Cell>
-                </Table.Row>
-              ))}
-              <Table.Row className="border-gray-600">
-                <Table.Cell></Table.Cell>
-                <Table.Cell></Table.Cell>
-                <Table.Cell className="font-bold underline">
-                  Suma Total
-                </Table.Cell>
-                <Table.Cell>{formatNumber.format(totalSum)}</Table.Cell>
-              </Table.Row>
-            </>
-          )} */}
         </Table.Body>
       </Table>
     </section>
